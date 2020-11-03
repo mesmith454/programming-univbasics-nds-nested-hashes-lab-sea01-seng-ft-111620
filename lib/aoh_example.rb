fa1cdd8a9de6b6fe5c133d1b95c8307f09023f2d
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-03 21:35:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  sort([DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G])
+  [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
   
 end
 
