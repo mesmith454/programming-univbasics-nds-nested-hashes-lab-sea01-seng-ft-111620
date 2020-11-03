0d8c738fcb70d0da8aff3265b56e8f6f9094b3eb
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-03 21:29:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,9 +13,8 @@ def assembled_aoh
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
-  constants
 end
-
+assembled_aoh
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
